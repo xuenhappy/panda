80ee5d62b04e67b54ad3f3df9dcf3d0fb3a695e3
xuen <xuen@M6800>
2019-12-23 00:11:35 +0800

    add recognizer

---
+++ src/main/java/org/bamboo/nlp/panda/core/TTCellRecognizer.java
@@ -0,0 +1,17 @@
+package org.bamboo.nlp.panda.core;
+
+/**
+ * 2、3中文组合构词器，这个识别器组合所有可能的长度为2和长度为3的中文词组
+ * 
+ * @author xuen
+ *
+ */
+public class TTCellRecognizer implements CellRecognizer {
+
+	@Override
+	public void read(CharSequence baseStr, CellMap map) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
