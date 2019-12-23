02aa55cb6f9a03a5f88daac4a0bf9b83743c91a2
xuen <xuen@mokahr.com>
2019-12-23 11:12:50 +0800

    add impl

--- src/main/java/org/bamboo/nlp/panda/tools/NeuralNetwork.java
+++ src/main/java/org/bamboo/nlp/panda/tools/NeuralNetwork.java
@@ -36,8 +36,8 @@ public final class NeuralNetwork {
 			public FloatMatrix dothis(FloatMatrix val) {
 				return MatrixFunctions.tanhi(val);
 			}
-		}), 
-		
+		}),
+
 		/**
 		 * sigmoid激活
 		 */
@@ -47,7 +47,7 @@ public final class NeuralNetwork {
 				return MatrixFunctions.tanhi(val.divi(2)).addi(1).divi(2);
 			}
 		}),
-		
+
 		/**
 		 * exp激活
 		 */
@@ -65,23 +65,23 @@ public final class NeuralNetwork {
 	}
 
 	/**
-	 * dense层
+	 * dense层for torch
 	 * 
 	 * @author xuen
 	 *
 	 */
-	public static final class Dense {
+	public static final class TDense {
 		private final Activation activation;
 		private final FloatMatrix weight;
 		private final FloatMatrix blas;
 
-		public Dense(FloatMatrix weight, FloatMatrix blas, Activation activation) {
+		public TDense(FloatMatrix weight, FloatMatrix blas, Activation activation) {
 			this.activation = activation;
-			this.weight = weight;
+			this.weight = weight.transpose();
 			this.blas = blas;
 		}
 
-		public FloatMatrix call(FloatMatrix input) {
+		public FloatMatrix forward(FloatMatrix input) {
 			FloatMatrix val = input.mul(weight);
 			if (blas != null)
 				val = val.addiRowVector(blas);
@@ -98,20 +98,69 @@ public final class NeuralNetwork {
 	 * @author xuen
 	 *
 	 */
-	public static final class GRU {
+	public static final class TGRU {
+		private final FloatMatrix wIh;
+		private final FloatMatrix wHh;
+		private final FloatMatrix bIh;
+		private final FloatMatrix bHh;
 
-		public FloatMatrix zeroState() {
-			return null;
+		/**
+		 * 
+		 * @return
+		 */
+		public FloatMatrix zeroState(int batchSize) {
+			return FloatMatrix.zeros(batchSize, this.wHh.rows);
 		}
 
-		public FloatMatrix call(FloatMatrix state, FloatMatrix input) {
-			return null;
+		private FloatMatrix linear(FloatMatrix args, FloatMatrix weights, FloatMatrix biases) {
+			FloatMatrix x = args.mul(weights);
+			if (biases != null)
+				x = x.addiRowVector(biases);
+			return x;
 		}
 
+		public FloatMatrix forward(FloatMatrix state, FloatMatrix input) {
+			FloatMatrix gi =linear(input, wIh, bIh);
+	        FloatMatrix gh =linear(state,wHh, bHh);
+	        i_r, i_i, i_n = np.split(gi, 3, axis=1);
+	        h_r, h_i, h_n = np.split(gh, 3, axis=1);
+	        resetgate= sigmoid(i_r + h_r);
+	     
+	        inputgate = sigmoid(i_i + h_i);
+	        		FloatMatrix newgate =Activation.Tanh.act.dothis( (i_n + resetgate * h_n));
+	        FloatMatrix hy = newgate.addi( inputgate * (state - newgate))
+			
+			
+			return hy;
+		}
+
+		public TGRU(FloatMatrix wIh, FloatMatrix wHh, FloatMatrix bIh, FloatMatrix bHh) {
+			this.wHh = wHh.transpose();
+			this.wIh = wIh.transpose();
+			this.bHh = bHh;
+			this.bIh = bIh;
+		}
+
+		public int GetStateSize() {
+			return this.wHh.rows;
+		}
+
+		public FloatMatrix runGru(FloatMatrix input) {
+			/**
+			 * input: (timeSteps, tokenSize)
+			 */
+			FloatMatrix outputs = new FloatMatrix(input.rows, this.GetStateSize());
+			FloatMatrix state = this.zeroState(1);
+			for (int i = 0; i < input.rows; i++) {
+				state = forward(input.getRow(i), state);
+				outputs.putRow(i, state.dup());
+			}
+			return outputs;
+		}
 	}
 
 	public static void main(String[] args) {
-		FloatMatrix f1 = new FloatMatrix(new float[] { 1, 0, 3, 4 });
+		FloatMatrix f1 = new FloatMatrix(new float[][] { { 1, 0, 3, 4 } });
 		FloatMatrix f2 = new FloatMatrix(new float[][] { { 8, 0, 3, 4 }, { 2, 3, 4, 5 } });
 		f2.addiRowVector(f1);
 		System.out.println(f2);
