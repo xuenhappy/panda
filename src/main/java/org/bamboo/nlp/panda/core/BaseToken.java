333745f6cc7c0391b125d12601178f1b28933198
xuen <xuen@mokahr.com>
2019-12-24 19:54:16 +0800

    add basic method

--- src/main/java/org/bamboo/nlp/panda/core/BaseToken.java
+++ src/main/java/org/bamboo/nlp/panda/core/BaseToken.java
@@ -2,4 +2,16 @@ package org.bamboo.nlp.panda.core;
 
 public final class BaseToken {
 
+	/**
+	 * split the given data
+	 * @param strs
+	 * @return
+	 */
+	public static WordCellList splitStr(CharSequence strs) {
+		
+		
+		
+		
+		return new WordCellList(null);
+	}
 }
