90c4f4151ecda5aeb41505e5a7be88116cb92b87
xuen <xuen@mokahr.com>
2019-12-26 19:28:48 +0800

    reduce code

--- src/main/java/org/bamboo/nlp/panda/tools/DoubleArrayTrie.java
+++ src/main/java/org/bamboo/nlp/panda/tools/DoubleArrayTrie.java
@@ -2,279 +2,136 @@ package org.bamboo.nlp.panda.tools;
 
 import java.util.*;
 
-
 /**
- * a double array trie 
+ * a double array trie
+ * 
  * @author xuen
  *
  * @param <V>
  */
 public class DoubleArrayTrie<V> {
-
 	/**
-	 * check array of the Double Array Trie structure
-	 */
-	protected int[] check;
-	/**
-	 * base array of the Double Array Trie structure
-	 */
-	protected int[] base;
-	/**
-	 * fail table of the Aho Corasick automata
-	 */
-	protected int[] fail;
-	/**
-	 * output table of the Aho Corasick automata
-	 */
-	protected int[][] output;
-	/**
-	 * outer value array
-	 */
-	protected V[] v;
-
-	/**
-	 * the length of every key
-	 */
-	protected int[] l;
-
-	/**
-	 * the size of base and check array
-	 */
-	protected int size;
-
-	/**
-	 * the char seq code map;
-	 */
-	protected Map<CharSequence, Integer> codeMap;
-
-	/**
-	 * buid code map
+	 * build array trie tmp used
 	 * 
-	 * @param datas
-	 */
-	protected int buildCodeMap(List<? extends StrList> datas) {
-		int max_code = 0;
-		if (codeMap == null)
-			codeMap = new HashMap<CharSequence, Integer>(2000);
-		for (StrList ent : datas) {
-			for (CharSequence seq : ent) {
-				if (codeMap.containsKey(seq))
-					continue;
-				codeMap.put(seq, codeMap.size() + 1);
-			}
-			max_code += ent.size();
-		}
-		return max_code + datas.size();
-	}
-
-	protected Integer getCode(CharSequence seq) {
-		if (codeMap.containsKey(seq))
-			return codeMap.get(seq);
-		return codeMap.size() + 1;
-	}
-
-	/**
-	 * Parse text
+	 * @author xuen
 	 *
-	 * @param text      The text
-	 * @param processor A processor which handles the output
 	 */
-	public void parseText(StrList text, IHit<V> processor) {
-		int position = 1;
-		int currentState = 0;
-		for (CharSequence seq : text) {
-			currentState = getState(currentState, getCode(seq));
-			int[] hitArray = output[currentState];
-			if (hitArray == null) {
-				++position;
-				continue;
-			}
+	private static class State {
 
-			for (int hit : hitArray) {
-				if (!processor.hit(position - l[hit], position, v[hit]))
-					return;
-			}
-			++position;
-		}
-	}
-
-	/**
-	 * Checks that string contains at least one substring
-	 *
-	 * @param text source text to check
-	 * @return {@code true} if string contains at least one substring
-	 */
-	public boolean matches(StrList text) {
-		int currentState = 0;
-		for (CharSequence chr : text) {
-			currentState = getState(currentState, getCode(chr));
-			int[] hitArray = output[currentState];
-			if (hitArray != null)
-				return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Get value by a String key, just like a map.get() method
-	 *
-	 * @param key The key
-	 * @return value if exist otherwise it return null
-	 */
-	public V get(StrList key) {
-		int index = exactMatchSearch(key);
-		if (index >= 0)
-			return v[index];
-		return null;
-	}
-
-	/**
-	 * Update a value corresponding to a key
-	 *
-	 * @param key   the key
-	 * @param value the value
-	 * @return successful or not（failure if there is no key）
-	 */
-	public boolean set(StrList key, V value) {
-		int index = exactMatchSearch(key);
-		if (index >= 0) {
-			v[index] = value;
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Pick the value by index in value array <br>
-	 * Notice that to be more efficiently, this method DO NOT check the parameter
-	 *
-	 * @param index The index
-	 * @return The value
-	 */
-	public V get(int index) {
-		return v[index];
-	}
-
-	/**
-	 * Processor handles the output when hit a keyword
-	 */
-	public interface IHit<V> {
 		/**
-		 * Hit a keyword, you can use some code like text.substring(begin, end) to get
-		 * the keyword
-		 *
-		 * @param begin the beginning index, inclusive.
-		 * @param end   the ending index, exclusive.
-		 * @param value the value assigned to the keyword
-		 * @return Return true for continuing the search and false for stopping it.
+		 * the depth of this state
 		 */
-		boolean hit(int begin, int end, V value);
-	}
+		protected final int depth;
 
-	/**
-	 * transmit state, supports failure function
-	 *
-	 * @param currentState
-	 * @param character
-	 * @return
-	 */
-	private int getState(int currentState, int character) {
-		int newCurrentState = transitionWithRoot(currentState, character); // 先按success跳转
-		while (newCurrentState == -1) // 跳转失败的话，按failure跳转
-		{
-			currentState = fail[currentState];
-			newCurrentState = transitionWithRoot(currentState, character);
-		}
-		return newCurrentState;
-	}
+		/**
+		 * fail state,if no match
+		 */
+		private State failure = null;
 
-	/**
-	 * transition of a state, if the state is root and it failed, then returns the
-	 * root
-	 *
-	 * @param nodePos
-	 * @param c
-	 * @return
-	 */
-	protected int transitionWithRoot(int nodePos, int c) {
-		int b = base[nodePos];
-		int p;
+		/**
+		 * the effective state
+		 */
+		private Set<Integer> emits = null;
+		/**
+		 * goto map
+		 */
+		private Map<Integer, State> success = new TreeMap<Integer, State>();
 
-		p = b + c + 1;
-		if (b != check[p]) {
-			if (nodePos == 0)
-				return 0;
-			return -1;
+		/**
+		 * index of in array
+		 */
+		private int index;
+
+		public State() {
+			this(0);
 		}
 
-		return p;
-	}
-
-	/**
-	 * Build a AhoCorasickDoubleArrayTrie from a map
-	 *
-	 * @param map a map containing key-value pairs
-	 */
-	public void build(List<? extends StrList> keys, List<V> tags) {
-		new Builder().build(keys, tags);
-	}
-
-	/**
-	 * match exactly by a key
-	 *
-	 * @param key the key
-	 * @return the index of the key, you can use it as a perfect hash function
-	 */
-	public int exactMatchSearch(StrList key) {
-		return exactMatchSearch(key, 0, 0, 0);
-	}
-
-	/**
-	 * match exactly by a key
-	 *
-	 * @param key
-	 * @param pos
-	 * @param len
-	 * @param nodePos
-	 * @return
-	 */
-	private int exactMatchSearch(StrList key, int pos, int len, int nodePos) {
-		if (len <= 0)
-			len = key.size();
-		if (nodePos <= 0)
-			nodePos = 0;
-
-		int result = -1;
-
-		return getMatched(pos, len, result, key, base[nodePos]);
-	}
-
-	private int getMatched(int pos, int len, int result, StrList keyChars, int b1) {
-		int b = b1;
-		int p;
-
-		for (CharSequence chr : keyChars) {
-			p = b + getCode(chr) + 1;
-			if (b == check[p])
-				b = base[p];
-			else
-				return result;
+		public State(int depth) {
+			this.depth = depth;
 		}
 
-		p = b; // transition through '\0' to check if it's the end of a word
-		int n = base[p];
-		if (b == check[p]) // yes, it is.
-		{
-			result = -n - 1;
+		public int getDepth() {
+			return this.depth;
 		}
-		return result;
-	}
 
-	/**
-	 * @return the size of the keywords
-	 */
-	public int size() {
-		return v.length;
+		public void addEmit(int keyword) {
+			if (this.emits == null)
+				this.emits = new TreeSet<Integer>(Collections.reverseOrder());
+			this.emits.add(keyword);
+		}
+
+		public Integer getLargestValueId() {
+			if (emits == null || emits.size() == 0)
+				return null;
+			return emits.iterator().next();
+		}
+
+		public void addEmit(Collection<Integer> emits) {
+			for (Integer emit : emits)
+				addEmit(emit);
+		}
+
+		public Collection<Integer> emit() {
+			return this.emits == null ? Collections.<Integer>emptyList() : this.emits;
+		}
+
+		public boolean isAcceptable() {
+			return this.depth > 0 && this.emits != null;
+		}
+
+		public State failure() {
+			return this.failure;
+		}
+
+		public void setFailure(State failState, int fail[]) {
+			this.failure = failState;
+			fail[index] = failState.index;
+		}
+
+		private State nextState(Integer character, boolean ignoreRootState) {
+			State nextState = this.success.get(character);
+			if (!ignoreRootState && nextState == null && this.depth == 0) {
+				nextState = this;
+			}
+			return nextState;
+		}
+
+		public State nextState(Integer character) {
+			return nextState(character, false);
+		}
+
+		public State nextStateIgnoreRootState(Integer character) {
+			return nextState(character, true);
+		}
+
+		public State addState(int character) {
+			State nextState = nextStateIgnoreRootState(character);
+			if (nextState == null) {
+				nextState = new State(this.depth + 1);
+				this.success.put(character, nextState);
+			}
+			return nextState;
+		}
+
+		public Collection<State> getStates() {
+			return this.success.values();
+		}
+
+		public Collection<Integer> getTransitions() {
+			return this.success.keySet();
+		}
+
+		public Map<Integer, State> getSuccess() {
+			return success;
+		}
+
+		public int getIndex() {
+			return index;
+		}
+
+		public void setIndex(int index) {
+			this.index = index;
+		}
 	}
 
 	/**
@@ -313,15 +170,36 @@ public class DoubleArrayTrie<V> {
 			int max_code = buildCodeMap(keys);
 			v = (V[]) tags.toArray();
 			l = new int[v.length];
-			// 构建二分trie树
+			// build tire
 			addAllKeyword(keys);
-			// 在二分trie树的基础上构建双数组trie树
+			// build double array tire base on tire
 			buildDoubleArrayTrie(max_code);
 			used = null;
-			// 构建failure表并且合并output表
+			// build failure table and merge output table
 			constructFailureStates();
 			rootState = null;
 			loseWeight();
+		}
+
+		/**
+		 * buid code map
+		 * 
+		 * @param datas
+		 */
+		protected int buildCodeMap(List<? extends StrList> datas) {
+			int max_code = 0;
+			if (codeMap == null)
+				codeMap = new HashMap<CharSequence, Integer>(2000);
+			for (StrList ent : datas) {
+				for (CharSequence seq : ent) {
+					if (codeMap.containsKey(seq))
+						continue;
+					codeMap.put(seq, codeMap.size() + 1);
+				}
+				max_code += ent.size();
+			}
+			// Estimated code table size
+			return (int) ((max_code + datas.size()) / 1.5) + 1;
 		}
 
 		/**
@@ -514,8 +392,7 @@ public class DoubleArrayTrie<V> {
 				List<Map.Entry<Integer, State>> new_siblings = new ArrayList<Map.Entry<Integer, State>>(
 						sibling.getValue().getSuccess().entrySet().size() + 1);
 
-				if (fetch(sibling.getValue(), new_siblings) == 0) // 叶子节点
-				{
+				if (fetch(sibling.getValue(), new_siblings) == 0) {// 叶子节点
 					base[begin + sibling.getKey()] = (-sibling.getValue().getLargestValueId() - 1);
 					progress++;
 				} else {
@@ -539,4 +416,243 @@ public class DoubleArrayTrie<V> {
 			check = ncheck;
 		}
 	}
+
+	/**
+	 * Processor handles the output when hit a keyword
+	 */
+	public static interface IHit<V> {
+		/**
+		 * Hit a keyword, you can use some code like text.substring(begin, end) to get
+		 * the keyword
+		 *
+		 * @param begin the beginning index, inclusive.
+		 * @param end   the ending index, exclusive.
+		 * @param value the value assigned to the keyword
+		 * @return Return true for continuing the search and false for stopping it.
+		 */
+		boolean hit(int begin, int end, V value);
+	}
+
+	/**
+	 * check array of the Double Array Trie structure
+	 */
+	private int[] check;
+	/**
+	 * base array of the Double Array Trie structure
+	 */
+	private int[] base;
+	/**
+	 * fail table of the Aho Corasick automata
+	 */
+	private int[] fail;
+	/**
+	 * output table of the Aho Corasick automata
+	 */
+	private int[][] output;
+	/**
+	 * outer value array
+	 */
+	private V[] v;
+
+	/**
+	 * the length of every key
+	 */
+	private int[] l;
+
+	/**
+	 * the size of base and check array
+	 */
+	private int size;
+
+	/**
+	 * the char seq code map;
+	 */
+	private Map<CharSequence, Integer> codeMap;
+
+	/**
+	 * code map
+	 * 
+	 * @param seq
+	 * @return
+	 */
+	private Integer getCode(CharSequence seq) {
+		if (codeMap.containsKey(seq))
+			return codeMap.get(seq);
+		return codeMap.size() + 1;
+	}
+
+	/**
+	 * Parse text
+	 *
+	 * @param text      The text
+	 * @param processor A processor which handles the output
+	 */
+	public void parseText(StrList text, IHit<V> processor) {
+		int position = 1;
+		int currentState = 0;
+		for (CharSequence seq : text) {
+			currentState = getState(currentState, getCode(seq));
+			int[] hitArray = output[currentState];
+			if (hitArray == null) {
+				++position;
+				continue;
+			}
+
+			for (int hit : hitArray) {
+				if (!processor.hit(position - l[hit], position, v[hit]))
+					return;
+			}
+			++position;
+		}
+	}
+
+	/**
+	 * Checks that string contains at least one substring
+	 *
+	 * @param text source text to check
+	 * @return {@code true} if string contains at least one substring
+	 */
+	public boolean matches(StrList text) {
+		int currentState = 0;
+		for (CharSequence chr : text) {
+			currentState = getState(currentState, getCode(chr));
+			int[] hitArray = output[currentState];
+			if (hitArray != null)
+				return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Get value by a String key, just like a map.get() method
+	 *
+	 * @param key The key
+	 * @return value if exist otherwise it return null
+	 */
+	public V get(StrList key) {
+		int index = exactMatchSearch(key);
+		if (index >= 0)
+			return v[index];
+		return null;
+	}
+
+	/**
+	 * Update a value corresponding to a key
+	 *
+	 * @param key   the key
+	 * @param value the value
+	 * @return successful or not（failure if there is no key）
+	 */
+	public boolean set(StrList key, V value) {
+		int index = exactMatchSearch(key);
+		if (index >= 0) {
+			v[index] = value;
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * transmit state, supports failure function
+	 *
+	 * @param currentState
+	 * @param character
+	 * @return
+	 */
+	private int getState(int currentState, int character) {
+		int newCurrentState = transitionWithRoot(currentState, character); // 先按success跳转
+		while (newCurrentState == -1) // 跳转失败的话，按failure跳转
+		{
+			currentState = fail[currentState];
+			newCurrentState = transitionWithRoot(currentState, character);
+		}
+		return newCurrentState;
+	}
+
+	/**
+	 * transition of a state, if the state is root and it failed, then returns the
+	 * root
+	 *
+	 * @param nodePos
+	 * @param c
+	 * @return
+	 */
+	private int transitionWithRoot(int nodePos, int c) {
+		int b = base[nodePos];
+		int p;
+
+		p = b + c + 1;
+		if (b != check[p]) {
+			if (nodePos == 0)
+				return 0;
+			return -1;
+		}
+
+		return p;
+	}
+
+	/**
+	 * Build a AhoCorasickDoubleArrayTrie from a map
+	 *
+	 * @param map a map containing key-value pairs
+	 */
+	public void build(List<? extends StrList> keys, List<V> tags) {
+		new Builder().build(keys, tags);
+	}
+
+	/**
+	 * match exactly by a key
+	 *
+	 * @param key the key
+	 * @return the index of the key, you can use it as a perfect hash function
+	 */
+	public int exactMatchSearch(StrList key) {
+		return exactMatchSearch(key, 0, 0, 0);
+	}
+
+	/**
+	 * match exactly by a key
+	 *
+	 * @param key
+	 * @param pos
+	 * @param len
+	 * @param nodePos
+	 * @return
+	 */
+	private int exactMatchSearch(StrList key, int pos, int len, int nodePos) {
+		if (len <= 0)
+			len = key.size();
+		if (nodePos <= 0)
+			nodePos = 0;
+		int result = -1;
+		return getMatched(pos, len, result, key, base[nodePos]);
+	}
+
+	private int getMatched(int pos, int len, int result, StrList keyChars, int b1) {
+		int b = b1;
+		int p;
+
+		for (CharSequence chr : keyChars) {
+			p = b + getCode(chr) + 1;
+			if (b == check[p])
+				b = base[p];
+			else
+				return result;
+		}
+
+		p = b; // transition through '\0' to check if it's the end of a word
+		int n = base[p];
+		if (b == check[p]) {// yes, it is.
+			result = -n - 1;
+		}
+		return result;
+	}
+
+	/**
+	 * @return the size of the keywords
+	 */
+	public int size() {
+		return v.length;
+	}
+
 }
