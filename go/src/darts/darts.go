5f49576bd479286268b813bf4ce947963e87660a
xuen <xuen@mokahr.com>
2020-12-31 15:48:32 +0800

    fix code site

---
+++ go/src/darts/darts.go
@@ -0,0 +1,55 @@
+package darts
+import (
+	"github.com/deckarep/golang-set"
+	"gonum.org/v1/gonum/mat"
+)
+// Atom is  the small cell in split
+type Atom struct {
+	image   string     //image of the atom
+	st, end int32      // [start,end) of this atom in ori string
+	tags    mapset.Set // type is
+}
+
+
+//WCell is the split token parttern
+type WCell struct{
+	word *Atom //the word in ori string
+	st,et int32 //this cell [start,end) in the atom list
+	emb mat.Vector // matrix os this
+	feat uint16 //represent which type of this word this is used for tagger
+}
+
+
+//map cursor used 
+type _Cursor struct{
+	pre,lack *_Cursor // pointer of this node pre and lack
+	val *WCell // val of the node  
+	idx uint32 // index of this course in list
+}
+
+
+type CellMap struct{
+	head _Cursor //this cellMap size
+	rows,colums,size uint32 // this cellmap countor
+}
+
+
+
+
+
+//regcinzer
+type CellRecognizer interface{
+	//recognizer all Wcell possable in the atomlist
+	read(content *Atom,size uint32, cmap *CellMap) 
+}
+
+type CellPresenter interface{
+	//set the cmap val data embeding
+	embed(context *Atom, size uint32,cmap *CellMap)
+}
+
+//interface of a prepre
+type CellQuantizer interface{
+	//distance of the pre and next cell
+	distance(pre WCell, next WCell) float32
+}
---
+++ go/src/go.mod
@@ -0,0 +1,14 @@
+module panda
+
+go 1.15
+
+require (
+	github.com/ajstarks/svgo v0.0.0-20200725142600-7a3c8b57fecb // indirect
+	github.com/go-latex/latex v0.0.0-20201220152147-94de1316b515 // indirect
+	golang.org/x/exp v0.0.0-20201229011636-eab1b5eb1a03 // indirect
+	golang.org/x/text v0.3.4 // indirect
+	golang.org/x/tools v0.0.0-20201230224404-63754364767c // indirect
+	gonum.org/v1/gonum v0.8.2 // indirect
+	gonum.org/v1/netlib v0.0.0-20201012070519-2390d26c3658 // indirect
+	gonum.org/v1/plot v0.8.1 // indirect
+)
