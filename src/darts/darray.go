ab07f8ab165d03919163029cabad04cdd089ad5d
xuen <xuen@mokahr.com>
2021-01-05 15:50:50 +0800

    fix darry

--- src/darts/darray.go
+++ src/darts/darray.go
@@ -12,401 +12,405 @@ package darts
  * Copyright 2021 - 2020 Your Company, Moka
  */
 
-
 import (
-	"sort"
 	"container/list"
+	"sort"
+
 	"github.com/emirpasic/gods/maps/treemap"
+	"gonum.org/v1/gonum/integrate"
 )
 
-//==================================================================================Basic
-const UINT32_MAX = ^uint32(0)
-
-type StringIter interface{
-	Next() *string // a string iter
+//StringIter is Used for parer input
+type StringIter interface {
+	//Next has next word
+	Next() bool
+	//Word is string
+	Word() *string
+	//Postion postion
+	Postion() int
 }
 
-type KVIter interface{
-	Next() (StringIter, uint32) // k,v iter
+//StrLabelIter is a input
+type StrLabelIter interface {
+	//Next is hase next pair
+	Next() bool
+	//String is next string
+	String() StringIter
+	//String is next value
+	Label() int
 }
 
- //===================================================================================Double Array Trie
-type Trie struct{
-	check,base,fail,l []uint32
-	v []uint32
-	output [][]uint32
-	maxlen uint32
-	codemap map[string]uint32
+//Trie is a double array trie
+type Trie struct {
+	check, base, fail, l []int
+	v                    []int
+	output               [][]int
+	maxlen               int
+	codemap              map[string]int
 }
 
 // get string code
-func (t *Trie) getcode(word *string) uint32{
-	code,ok :=t.codemap[*word]
-	if(!ok){
-		return len(t.codemap)+1
+func (t *Trie) getcode(word *string) int {
+	code, ok := t.codemap[*word]
+	if !ok {
+		return len(t.codemap) + 1
 	}
 	return code
 }
 
 //trans
-func (t *Trie) transitionWithRoot(nodePos uint32,  c uint32) uint32{
+func (t *Trie) transitionWithRoot(nodePos int, c int) int {
 	b := t.base[nodePos]
 	p := b + c + 1
-	if (b != t.check[p]){
-		if (nodePos == 0){
+	if b != t.check[p] {
+		if nodePos == 0 {
 			return 0
 		}
-		return UINT64_MAX
+		return -1
 	}
 	return p
 }
 
 // get state transpose
-func (t *Trie) getState(currentState uint32, character uint32) uint32{
+func (t *Trie) getstate(currentState int, character int) int {
 	newCurrentState := t.transitionWithRoot(currentState, character)
-	for{
-		if newCurrentState != UINT32_MAX {
-			break
-		}
+	for newCurrentState == -1 {
 		currentState = t.fail[currentState]
 		newCurrentState = t.transitionWithRoot(currentState, character)
 	}
 	return newCurrentState
 }
 
-
-//parse a text list hit ( [start,end),tagidx)
-func (t *Trie) ParseText(text StringIter, hit func(uint32,uint32,uint32)){
-	currentState := 0
-	index_bufer:=[t.maxlen]uint32{}
-	index_buffer_pos := 0
-	for seq,skip:=text.netext();seq!=nil;seq,skip:=text.netext(){
-		if skip{
-			continue
+//ParseText parse a text list hit ( [start,end),tagidx)
+func (t *Trie) ParseText(text StringIter, hit func(int, int, int)) {
+	currentState, indexBufferPos := 0, 0
+	indexBufer := make([]int, t.maxlen)
+	for text.Next() {
+		position := text.Postion()
+		seq := text.Word()
+		indexBufer[indexBufferPos%t.maxlen] = position
+		indexBufferPos++
+		currentState = t.getstate(currentState, t.getcode(seq))
+		hitArray := t.output[currentState]
+		for _, h := range hitArray {
+			preIndex := (indexBufferPos - t.l[h]) % t.maxlen
+			hit(indexBufer[preIndex], position+1, t.v[h])
 		}
-		index_bufer[index_buffer_pos % t.maxlen] = position
-		index_buffer_pos += 1
-		currentState = t.getState(currentState, t.getCode(seq))
-		hitArray = t.output[currentState]
-		for _,h:= range hitArray{
-			pre_index := (index_buffer_pos-t.l[h]) % t.maxlen
-			hit(index_bufer[pre_index], position+1, t.v[h])
-		}		
 	}
 }
 
-
-
-//=========================================================================================DFA
-//DFA mechine state
-type State struct{
-	depth int // the string length 
-	failure *State // match failed use
-	emits []uint32 // emits
+//State is DFA mechine state
+type State struct {
+	depth   int          // the string length
+	failure *State       // match failed use
+	emits   []int        // emits
 	success *treemap.Map //go map
-	index uint32 //index of the struct
+	index   int          //index of the struct
 }
 
-
-func newState(int depth){
+func newState(depth int) *State {
 	S := new(State)
-	S.depth=depth
-	S.success=treemap.NewWithIntComparator()
+	S.depth = depth
+	S.success = treemap.NewWithIntComparator()
 	return S
 }
 
 //insert sort a sort to keep it order
-func insertSorted(s []int, e int)[]int{
-	i := sort.Search(len(s), func(i int) bool { return (s)[i] <=e })
-	if i==len(s){
-		s=append(s,e)
+func insertSorted(s []int, e int) []int {
+	i := sort.Search(len(s), func(i int) bool { return (s)[i] <= e })
+	if i == len(s) {
+		s = append(s, e)
 		return s
 	}
-	if s[i]==e{
+	if s[i] == e {
 		return s
 	}
-	s=append(s, 0)
+	s = append(s, 0)
 	copy(s[i+1:], s[i:])
 	s[i] = e
 	return s
-}   
+}
 
 //add emit
-func (s *State) addEmit(keyword uint32){
-	if keyword!=UINT32_MAX{
-		s.emits=insertSorted(s.emits,keyword)
+func (s *State) addEmit(keyword int) {
+	if keyword != -100000 {
+		s.emits = insertSorted(s.emits, keyword)
 	}
 }
 
 //get l code
-func (s *State) getLargestValueId()uint32{
-	if len(s.emits)<1{
-		return UINT32_MAX
+func (s *State) getMaxValueIDgo() int {
+	if len(s.emits) < 1 {
+		return -100000
 	}
 	return s.emits[0]
 }
 
-func (s *State) isAcceptable(){
-	return s.depth > 0 && len(s.emits)>0
+func (s *State) isAcceptable() bool {
+	return s.depth > 0 && len(s.emits) > 0
 }
 
-func (s *State) setFailure(failState *State, fail []uint64){
+func (s *State) setFailure(failState *State, fail []int) {
 	s.failure = failState
 	fail[s.index] = failState.index
 }
 
-func (s *State) nextState(character uint32, ignoreRootState bool) *State{
-	nextState,_:=s.success.Get(character)
-	if (!ignoreRootState) && (nextState==nil) && (s.depth == 0){
+func (s *State) nextState(character int, ignoreRootState bool) *State {
+	nextState, _ := s.success.Get(character)
+	if (!ignoreRootState) && (nextState == nil) && (s.depth == 0) {
 		nextState = s
 	}
-	if(nextState==nil){
+	if nextState == nil {
 		return nil
 	}
 	return nextState.(*State)
 }
 
-func (s *State) nextState(character uint32) *State{
-	return s.nextState(character,false)
-}
-
-func (s *State) addState(character uint32) *State{
+func (s *State) addState(character int) *State {
 	nextS := s.nextState(character, true)
-	if nextS==nil{
-		nextS = newState(s.depth+1)
-		s.success.Put(character,nextS)
+	if nextS == nil {
+		nextS = newState(s.depth + 1)
+		s.success.Put(character, nextS)
 	}
 	return nextS
 }
 
-
-
-//=============================================================================Builder
-type Builder struct{
+//Builder is inner useed
+type Builder struct {
 	rootState *State
-	trie *Trie
+	trie      *Trie
 	/**
 	* whether the position has been used
-	*/
+	 */
 	used []bool
 	/**
 	* the allocSize of the dynamic array
-	*/
-	allocSize uint32
+	 */
+	allocSize int
 	/**
 	* a parameter controls the memory growth speed of the dynamic array
-	*/
-	progress uint32
+	 */
+	progress int
 	/**
 	* the next position to check unused memory
-	*/
-	nextCheckPos uint32
+	 */
+	nextCheckPos int
 	/**
 	* the size of the key-pair sets
-	*/
-	size uint32
+	 */
+	size int
 }
 
-
-
 //zip no use data
-func (b *Builder) zipWeight(){
-	msize=b.size+len(b.codemap)+1
-	newBase:=make([]uint32,msize,msize)
-	copy(b.trie.base[:b.size],newBase[:b.size])
+func (b *Builder) zipWeight() {
+	msize := b.size + int(len(b.trie.codemap)) + 1
+	newBase := make([]int, msize)
+	copy(b.trie.base[:b.size], newBase[:b.size])
 	b.trie.base = newBase
 
-	newCheck :=make([]uint32,msize,msize)
-	copy(b.trie.check[:b.size],newCheck[:b.size])
+	newCheck := make([]int, msize, msize)
+	copy(b.trie.check[:b.size], newCheck[:b.size])
 	b.trie.check = newCheck
 }
 
+func (b *Builder) constructFailureStates() {
+	b.trie.fail = make([]int, b.size+1)
+	b.trie.output = make([][]int, b.size+1)
+	queue := list.New()
 
-
-func (b * Builder) constructFailureStates(){
-	b.trie.fail=make([]uint32,b.size+1)
-	b.trie.output=make([][]uint32,b.size+1)
-	queue:=list.New()
-	
-	for state:=range b.rootState.success.Values(){
-		queue.PushBack(state)
-		b.trie.output[targetState.index] = state.(*State).emits
+	cpy := func(ori []int) []int {
+		dest := make([]int, len(ori))
+		copy(ori, dest)
+		return dest
 	}
-	for{
-		if queue.Len()<1{
-			break
+
+	for _, state := range b.rootState.success.Values() {
+		depthOneState := state.(*State)
+		depthOneState.setFailure(b.rootState, b.trie.fail)
+		queue.PushBack(state)
+		if len(depthOneState.emits) > 0 {
+			b.trie.output[depthOneState.index] = cpy(depthOneState.emits)
 		}
-		currentState:=queue.Remove(queue.Front()).(*State)
-		for key:=range b.rootState.success.Keys(){
-			transition:=key.(uint32)
-			targetState:= currentState.nextState(transition)
+
+	}
+	for queue.Len() > 0 {
+		currentState := queue.Remove(queue.Front()).(*State)
+		for _, key := range b.rootState.success.Keys() {
+			transition := key.(int)
+			targetState := currentState.nextState(transition, false)
 			queue.PushBack(targetState)
-			traceFailureState:= currentState.failure
-			for{
-				if(traceFailureState.nextState(transition)!=nil){
-					break
-				}
+			traceFailureState := currentState.failure
+			for traceFailureState.nextState(transition, false) == nil {
 				traceFailureState = traceFailureState.failure
 			}
-			newFailureState := traceFailureState.nextState(transition)
+			newFailureState := traceFailureState.nextState(transition, false)
 			targetState.setFailure(newFailureState, b.trie.fail)
-			for _,e :=range newFailureState.emits{
+			for _, e := range newFailureState.emits {
 				targetState.addEmit(e)
 			}
-			targetState.emits |= newFailureState.emits
-			b.trie.output[targetState.index] = targetState.emits
+			b.trie.output[targetState.index] = cpy(targetState.emits)
 		}
 	}
 }
 
-
-
-func (b *Builder)addAllKeyword(kvs KVIter){
-	max_code,index:= 0,-1
-	t:=b.trie
-	for{
-		index+=1
-		k,v:=kvs.Next()
-		if k==nil{
-			break
-		}
-		lens:=0
-		currentState:=b.rootState
-		for{
-			s:=k.Next()
-			if s==nil{
-				break
-			}
-			lens+=1
-			code:=b.trie.getcode(s)
-			t.codemap[*s]=code
-			currentState=currentState.addState(code)
+func (b *Builder) addAllKeyword(kvs StrLabelIter) int {
+	maxCode, index, t := 0, -1, b.trie
+	for kvs.Next() {
+		k, v := kvs.String(), kvs.Label()
+		index++
+		lens := 0
+		currentState := b.rootState
+		for k.Next() {
+			s := k.Word()
+			lens++
+			code := b.trie.getcode(s)
+			t.codemap[*s] = code
+			currentState = currentState.addState(code)
 		}
 		currentState.addEmit(index)
-		t.l=append(t.l,lens)
-		max_code += lens
-		t.v=append(t.v, v)
-
-		
-
+		t.l = append(t.l, int(lens))
+		maxCode += lens
+		t.v = append(t.v, v)
 	}
-	return int((max_code+len(t.codemap))/1.5) + 1
+	return int(float32(maxCode+len(t.codemap))/1.5) + 1
 }
 
 //resize data
-func (b *Builder) resize(newSize uint32){
-	base2 := make([]uint32,newSize,newSize)
-	check2 := make([]uint32,newSize,newSize)
-	used2 := make([]bool,newSize,newSize)
-	if b.allocSize > 0{
-		copy(b.trie.base[:int(b.allocSize)],base2[:int(b.allocSize)])
-		copy(b.trie.check[:int(b.allocSize)],check2[:int(b.allocSize)])
-		copy(b.used[:int(b.allocSize)],used2[:int(b.allocSize)])
+func (b *Builder) resize(newSize int) {
+	base2 := make([]int, newSize)
+	check2 := make([]int, newSize)
+	used2 := make([]bool, newSize)
+	if b.allocSize > 0 {
+		copy(b.trie.base[:int(b.allocSize)], base2[:int(b.allocSize)])
+		copy(b.trie.check[:int(b.allocSize)], check2[:int(b.allocSize)])
+		copy(b.used[:int(b.allocSize)], used2[:int(b.allocSize)])
 	}
-	b.trie.base=base2
-	b.trie.check=check2
-	b.used=used2
-	b.allocSize=newSize
+	b.trie.base = base2
+	b.trie.check = check2
+	b.used = used2
+	b.allocSize = newSize
 }
 
-
-
-func (b *Builder) insert(siblings){
-	begin = 0
-	pos = max(siblings[0][0] + 1, self.nextCheckPos) - 1
-	nonzero_num = 0
-	first = 0
-	if self.allocSize <= pos:
-		self.resize(pos + 1)
-	while True:
-		pos += 1
-		if self.allocSize <= pos:
-			self.resize(pos + 1)
-		if self.check[pos] != 0:
-			nonzero_num += 1
-			continue
-		elif first == 0:
-			self.nextCheckPos = pos
-			first = 1
-
-		begin = pos - siblings[0][0]
-		if self.allocSize <= (begin + siblings[-1][0]):
-			self.resize(begin + siblings[-1][0]+100)
-		if self.used[begin]:
-			continue
-		flag = False
-		for kv in siblings:
-			if self.check[begin + kv[0]] != 0:
-				flag = True
-				break
-
-		if not flag:
-			break
-
-	if 1.0 * nonzero_num/(pos - self.nextCheckPos + 1) >= 0.95:
-		self.nextCheckPos = pos
-	self.used[begin] = True
-
-	if self.size < begin + siblings[-1][0] + 1:
-		self.size = begin + siblings[-1][0] + 1
-
-	for kv in siblings:
-		self.check[begin + kv[0]] = begin
-
-	for kv in siblings:
-		new_siblings = self._fetch(kv[1])
-		if not new_siblings:
-			self.base[begin + kv[0]] = -(kv[1].getLargestValueId()+1)
-			self.progress += 1
-		else:
-			self.base[begin + kv[0]] = self._insert(new_siblings)
-		kv[1].index = begin + kv[0]
-	return begin
+//Pair si tmp used
+type Pair struct {
+	k interface{}
+	v interface{}
 }
 
-
-
-func fetch(parent *State){
-	siblings:=make([],parent.Size()+1)
-	if parent.isAcceptable():
+func fetch(parent *State) []Pair {
+	siblings := make([]Pair, 0, parent.success.Size()+1)
+	if parent.isAcceptable() {
 		fakeNode := newState(-(parent.depth + 1))
-		fakeNode.addEmit(parent.getLargestValueId())
-		siblings.append((0, fakeNode))
-	for (k, v) in sorted(parent.success.items(), key=lambda x: x[0]):
-		siblings.append((k + 1, v))
+		fakeNode.addEmit(parent.getMaxValueIDgo())
+		siblings = append(siblings, Pair{0, fakeNode})
+	}
+	it := parent.success.Iterator()
+	for it.Next() {
+		siblings = append(siblings, Pair{it.Key().(int) + 1, it.Value()})
+	}
 	return siblings
 }
 
-func (b *Builder) buildDoubleArrayTrie(max_code uint32){
-	b.resize(max_code+10)
-	b.trie.base[0]=1
-	root_node := b.rootState
-	siblings = self._fetch(root_node)
-	if not siblings:
-		return
-	self._insert(siblings)
+func (b *Builder) insert(queue *list.List) {
+	tCurrent := queue.Remove(queue.Front()).(Pair)
+	value, siblings := tCurrent.k.(int), tCurrent.v.([]Pair)
+
+	begin, nonZeroNum := 0, 0
+	first := true
+	pos := b.nextCheckPos - 1
+	if pos < siblings[0].k.(int) {
+		pos = siblings[0].k.(int)
+	}
+	if b.allocSize <= pos {
+		b.resize(pos + 1)
+	}
+	t := b.trie
+	for {
+		pos++
+		if b.allocSize <= pos {
+			b.resize(pos + 1)
+		}
+		if t.check[pos] != 0 {
+			nonZeroNum++
+			continue
+		} else if first {
+			b.nextCheckPos = pos
+			first = false
+		}
+
+		begin = pos - siblings[0].k.(int)
+		if b.allocSize <= (begin + siblings[len(siblings)-1].k.(int)) {
+			b.resize(begin + siblings[len(siblings)-1].k.(int) + 100)
+		}
+		if b.used[begin] {
+			continue
+		}
+		allIszero := true
+		for i := 0; i < len(siblings); i++ {
+			if t.check[begin+siblings[i].k.(int)] != 0 {
+				allIszero = false
+				break
+			}
+		}
+		if allIszero {
+			break
+		}
+	}
+
+	if float32(nonZeroNum)/float32(pos-b.nextCheckPos+1) >= 0.95 {
+		b.nextCheckPos = pos
+	}
+	b.used[begin] = true
+
+	if b.size < begin+siblings[len(siblings)-1].k.(int)+1 {
+		b.size = begin + siblings[len(siblings)-1].k.(int) + 1
+	}
+	for i := 0; i < len(siblings); i++ {
+		t.check[begin+siblings[i].k.(int)] = begin
+	}
+	for i := 0; i < len(siblings); i++ {
+		kv := siblings[i]
+		newSiblings := fetch(kv.v.(*State))
+		if len(newSiblings) < 1 {
+			t.base[begin+kv.k.(int)] = -(kv.v.(*State).getMaxValueIDgo() + 1)
+			b.progress++
+		} else {
+			queue.PushBack(Pair{begin + kv.k.(int), newSiblings})
+		}
+		kv.v.(*State).index = begin + kv.k.(int)
+	}
+	if value >= 0 {
+		t.base[value] = begin
+	}
 }
 
-
-
-func (b *Builder) build(kvs KVIter){
-	max_code := b.addAllKeyword(kvs)
+func (b *Builder) build(kvs StrLabelIter) {
+	maxCode := b.addAllKeyword(kvs)
 	//build double array tire base on tire
-	b.buildDoubleArrayTrie(max_code)
+	b.resize(maxCode + 10)
+	b.trie.base[0] = 1
+	siblings := fetch(b.rootState)
+	if len(siblings) > 0 {
+		queue := list.New()
+		queue.PushBack(Pair{-1, siblings})
+		for queue.Len() > 0 {
+			b.insert(queue)
+		}
+	}
 	//build failure table and merge output table
 	b.constructFailureStates()
 	b.rootState = nil
 	b.zipWeight()
 }
 
-//compile the data
-func Compile(kv_iter KVIter) *Trie{
+//Compile the data
+func Compile(kvs StrLabelIter) *Trie {
 	var builder Builder
-	builder.rootState=new(State)
-	builder.trie=new(Trie)
-	builder.build(kv_iter)
+	builder.rootState = new(State)
+	builder.trie = new(Trie)
+	builder.build(kvs)
 	//clear mem
-	result:=builder.trie
-	builder.trie=nil
-    return result
+	result := builder.trie
+	builder.trie = nil
+	return result
 }
--- src/go.mod
+++ src/go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/ajstarks/svgo v0.0.0-20200725142600-7a3c8b57fecb // indirect
 	github.com/deckarep/golang-set v1.7.1
+	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/go-latex/latex v0.0.0-20201220152147-94de1316b515 // indirect
 	golang.org/x/exp v0.0.0-20201229011636-eab1b5eb1a03 // indirect
 	golang.org/x/text v0.3.4 // indirect
