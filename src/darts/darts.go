76971323ea15f8fbbe8da8862d01474cdd8065c8
xuen <xuen@mokahr.com>
2021-01-06 23:33:29 +0800

    add codes

--- src/darts/darts.go
+++ src/darts/darts.go
@@ -7,42 +7,87 @@ import (
 
 // Atom is  the small cell in split
 type Atom struct {
-	image   string     //image of the atom
-	st, end int32      // [start,end) of this atom in ori string
-	tags    mapset.Set // type is
+	Image   string     //image of the atom
+	St, End int        // [start,end) of this atom in ori string
+	Tags    mapset.Set // type is
+}
+
+//NewAtom crate the atom
+func NewAtom(str *string, start, end int) *Atom {
+	atom := new(Atom)
+	atom.Image = *str
+	atom.St = start
+	atom.End = end
+	return atom
 }
 
 //WCell is the split token parttern
 type WCell struct {
-	word   *Atom      //the word in ori string
-	st, et int32      //this cell [start,end) in the atom list
-	emb    mat.Vector // matrix os this
-	feat   uint16     //represent which type of this word this is used for tagger
+	Word   *Atom      //the word in ori string
+	St, Et int        //this cell [start,end) in the atom list
+	Emb    mat.Vector // matrix os this
+	Feat   uint16     //represent which type of this word this is used for tagger
+}
+
+//NewWcell create a WCell
+func NewWcell(atom *Atom, s, e int) *WCell {
+	cell := new(WCell)
+	cell.Word = atom
+	cell.St = s
+	cell.Et = e
+	return cell
 }
 
 //Cursor map cursor used
 type Cursor struct {
 	pre, lack *Cursor // pointer of this node pre and lack
 	val       *WCell  // val of the node
-	idx       uint32  // index of this course in list
+	idx       int     // index of this course in list
+}
+
+func newCur(value *WCell, pre, next *Cursor) *Cursor {
+	cur := new(Cursor)
+	cur.val = value
+	cur.pre = pre
+	cur.lack = next
+	return cur
 }
 
 //CellMap used
 type CellMap struct {
-	head               Cursor //this cellMap size
-	rows, colums, size uint32 // this cellmap countor
+	head               *Cursor //this cellMap size
+	rows, colums, size int     // this cellmap countor
+}
+
+func (cmap *CellMap) indexMap() {
+	node := cmap.head
+	index := -1
+	for node.lack != nil {
+		node = node.lack
+		index++
+		node.idx = index
+	}
+}
+
+func newCellMap() *CellMap {
+	cmap := new(CellMap)
+	cmap.head = new(Cursor)
+	cmap.head.val = new(WCell)
+	cmap.head.val.St = -1
+	cmap.head.val.Et = 0
+	return cmap
 }
 
 //CellRecognizer is code
 type CellRecognizer interface {
 	//recognizer all Wcell possable in the atomlist
-	read(content *[]Atom, cmap *CellMap)
+	read(content []Atom, cmap *CellMap)
 }
 
 //CellPresenter is embed
 type CellPresenter interface {
 	//set the cmap val data embeding
-	embed(context *[]Atom, cmap *CellMap)
+	embed(context []Atom, cmap *CellMap)
 }
 
 //CellQuantizer interface of a prepre
